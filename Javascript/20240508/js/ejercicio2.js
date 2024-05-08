ad9f1b9dbaea5336785da9dea127c15bfcd496f8
Pablo Ruiz Soria <pabloruizsoria@gmail.com>
2024-05-08 18:45:03 +0200

    Solución del ejercicio 2

---
+++ Javascript/20240508/js/ejercicio2.js
@@ -0,0 +1,18 @@
+var entrada1 = prompt("Introduce un número entero");
+
+// Compruebo si el dato es válido
+if( entrada1 < 0 ){
+
+    console.log("El número es negativo");
+
+}else if( entrada1 > 0 ){
+
+    console.log("El número es positivo");
+
+}else if( entrada1 == 0 ){
+
+    console.log("El número es 0");
+    
+}else{
+    console.log("Combinación no contemplada");
+}
